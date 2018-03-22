001a0b706219948796653ef14e6c740b19083031
VamsiMohanRamineedi <vmr286@nyu.edu>
2018-03-22 18:02:06 -0400

    Multiple Inheritence

---
+++ multipleInheritence.py
@@ -0,0 +1,17 @@
+class Mother:
+	eye_color = 'brown'
+	hair_color = 'dark brown'
+	hair_type = 'curly'
+
+class Father:
+	eye_color = 'blue'
+	hair_color = 'blond'
+	hair_type = 'straight'
+
+class Child(Mother, Father):
+	pass
+
+# c = Child()
+# print(c.eye_color)
+
+help(Child)
